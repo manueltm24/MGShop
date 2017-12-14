52eab1bea13dfd47f14cfd5ee0278c556bdb5ec2
Gustavo J. Henriquez C <gustavojoseh@gmail.com>
2017-12-14 00:33:38 -0400

    Nuevas librerias agregadas y generacion de reporte pdf de facturas despachadas funcionando

--- grails-app/controllers/com/mgshop/controllers/CarritoController.groovy
+++ grails-app/controllers/com/mgshop/controllers/CarritoController.groovy
@@ -2,12 +2,20 @@ package com.mgshop.controllers
 
 import com.mgshop.domains.ArticuloCompra
 import com.mgshop.domains.Compra
+import com.mgshop.domains.Producto
 import com.mgshop.domains.seguridad.Perfil
 import com.mgshop.domains.seguridad.Usuario
-import com.mgshop.domains.Producto
-
 import com.mgshop.domains.seguridad.UsuarioPerfil
 import grails.plugin.springsecurity.annotation.Secured
+import net.sf.jasperreports.engine.JasperCompileManager
+import net.sf.jasperreports.engine.JasperFillManager
+import net.sf.jasperreports.engine.JasperPrint
+import net.sf.jasperreports.engine.JasperReport
+import net.sf.jasperreports.engine.export.JRPdfExporter
+import net.sf.jasperreports.export.*
+
+import java.sql.Connection
+import java.sql.DriverManager
 
 @Secured(["ROLE_ADMIN", "ROLE_USUARIO"])
 
@@ -84,10 +92,56 @@ class CarritoController {
         compra.estadoDespacho=true
         compra.modificadoPor=usuario.username
         compra.save(failOnError: true, flush: true)
+
+        realizarReportePDF()
+
         redirect(uri:"/carrito/despachoCompras")
+    }
 
+    def realizarReportePDF() {
+        String hostName = "localhost"
+        String dbName = "mgshopdev"
+        String userName = "mgshop"
+        String password = "123456"
 
+        String source_reporte = "/home/guhex/IdeaProjects/MGShop/mgshop_reports/factura.jrxml"
+        JasperReport jasperReport = JasperCompileManager.compileReport(source_reporte)
 
+        Class.forName("com.mysql.jdbc.Driver")
 
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName
+
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password)
+
+        // Parameters for report
+        Map<String, Object> parameters = new HashMap<String, Object>()
+
+        JasperPrint print = JasperFillManager.fillReport(jasperReport,
+                parameters, conn)
+
+        // Make sure the output directory exists.
+        File outDir = new File("/home/guhex/IdeaProjects/MGShop/facturas")
+        outDir.mkdirs()
+
+        // PDF Exportor.
+        JRPdfExporter exporter = new JRPdfExporter()
+
+        ExporterInput exporterInput = new SimpleExporterInput(print)
+        // ExporterInput
+        exporter.setExporterInput(exporterInput)
+
+        // ExporterOutput
+        OutputStreamExporterOutput exporterOutput = new SimpleOutputStreamExporterOutput(
+                "/home/guhex/IdeaProjects/MGShop/facturas/factura_" + new Date().toString() + ".pdf")
+        // Output
+        exporter.setExporterOutput(exporterOutput)
+
+        //
+        SimplePdfExporterConfiguration configuration = new SimplePdfExporterConfiguration()
+        exporter.setConfiguration(configuration)
+        exporter.exportReport()
+
+        System.out.print("Reporte exportado!")
     }
 }
+
---
+++ src/test/groovy/com/mgshop/controllers/REST/GraficosDataRestControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.mgshop.controllers.REST
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class GraficosDataRestControllerSpec extends Specification implements ControllerUnitTest<GraficosDataRestController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
